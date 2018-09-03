c8696606cde4cfd505910a6e5c6b231b85b19ad2
redsnow <kurdb14@gmail.com>
2018-09-03 03:21:04 +0300

    branch added comments deleted inside the src code file

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,46 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 
+Route::get('/tasks', function () {
+
+    $tasks = DB::table('tasks')->latest()->get(); 
+
+    return view('tasks.index', compact('tasks'));
+});
+
+
+/* maybe we need to foucs on specific task
+we seperate that task and in this example we use */
+
+
+/* this curly brace is a wild card that  
+{id} checks to see what used type in uri 
+or a link to 
+*/
+
+Route::get('/tasks/{id}', function ($id) {
+    
+
+
+$task = DB::table('tasks')->find($id); // fetch/retrieve data from tasks table
+    
+    //dd($task);
+
+    return view('tasks.show', compact('task'));
+
+});
+
+
+
 
 Route::get('/about', function () {
     
-    //some fetched data from database
-
     $tasks = DB::table('tasks') -> get(); // fetch data from tasks table
 
 
@@ -27,29 +48,11 @@ Route::get('/about', function () {
     //compact function is a larevel function that output array of data
     
     return view('about', compact('tasks'));
-    //return $tasks; RUN THIS to see it in JSON FORMAT
-
+    
 });
 
 
 
-//maybe we need to foucs on specific task
-//we seperate that task and in this example we use 
-
-Route::get('/tasks/{id}', function ($id) {
-    
-    //some fetched data from database
-
-    // add conditions to tasks
-
-   
-//var named $task is used for finding the id in this case and out put related data the availbe ID'S
-    $task = DB::table('tasks')->find($id); // fetch/retrieve data from tasks table
-    
-    //compact function is a larevel function that output array of data
-    
-    return view('task.show', compact('tasks'));
-});
 
 
 
